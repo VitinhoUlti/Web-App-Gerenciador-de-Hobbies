070bead39ac0eac66c3a0dddb295e35c7ec986c5
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2025-03-10 01:45:24 -0300

    retirando hobbiescontroller

--- obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf2a7db8537bb54bdbba99a778e3d7811446163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9cccc56ee91df70032969ccd5f8dd0977125c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
