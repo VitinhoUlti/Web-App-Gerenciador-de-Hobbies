94d5cf6e365a42da67614ec14a2268165f858c37
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2025-04-02 20:35:43 -0300

    Adicionando a view de editar e deletar

--- obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81263456c81f7601a4d697887c30627643f5253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e73cae89f674cb733bf908cf0b24fb93dfcacc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
